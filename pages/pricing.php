3eccb0a8091d96f606ab771a07594032f687ba13
Samuel Betio <38454185+lmas2969@users.noreply.github.com>
2018-07-26 17:28:27 +0800

    Create pricing.php

---
+++ pages/pricing.php
@@ -0,0 +1,260 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Learn | Pages | Pricing</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<script src="../assets/js/modernizr-2.6.2.min.js"></script>
+<!--[if lt IE 9]>
+<script src="../assets/js/respond.min.js"></script>
+<![endif]-->
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body>
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<div class="fh5co-loader"></div>
+<div id="page">
+  <nav class="fh5co-nav" role="navigation">
+    <div class="top">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 text-right">
+            <p class="num">Call: +01 123 456 7890</p>
+            <ul class="fh5co-social">
+              <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-twitter"></i></a></li>
+              <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-dribbble"></i></a></li>
+              <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-github"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="top-menu">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-1">
+            <div id="fh5co-logo"><a href="https://github.com/samuelbetio/dGitFile">Learn<span>.</span></a></div>
+          </div>
+          <div class="col-xs-11 text-right menu-1">
+            <ul>
+              <li><a href="https://github.com/samuelbetio/dGitFile">Home</a></li>
+              <li><a href="courses.php">Courses</a></li>
+              <li class="active"><a href="pricing.php">Pricing</a></li>
+              <li class="has-dropdown"><a href="blog.php">Blog</a>
+                <ul class="dropdown">
+                  <li><a href="https://github.com/samuelbetio/dGitFile">Web Design</a></li>
+                  <li><a href="https://github.com/samuelbetio/dGitFile">eCommerce</a></li>
+                  <li><a href="https://github.com/samuelbetio/dGitFile">Branding</a></li>
+                  <li><a href="https://github.com/samuelbetio/dGitFile">API</a></li>
+                </ul>
+              </li>
+              <li><a href="contact.php">Contact</a></li>
+              <li class="btn-cta"><a href="https://github.com/samuelbetio/dGitFile"><span>Login</span></a></li>
+              <li class="btn-cta"><a href="https://github.com/samuelbetio/dGitFile"><span>Create a Course</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(../assets/images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center">
+          <div class="display-t">
+            <div class="display-tc animate-box" data-animate-effect="fadeIn">
+              <h1>Pricing Plan</h1>
+              <h2>Dignissimos asperiores vitae velit veniam totam</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div id="fh5co-pricing">
+    <div class="container">
+      <div class="row">
+        <div class="pricing">
+          <div class="col-md-3 animate-box">
+            <div class="price-box">
+              <h2 class="pricing-plan">Starter</h2>
+              <div class="price"><sup class="currency">$</sup>9<small>/month</small></div>
+              <ul class="classes">
+                <li>15 English 101</li>
+                <li class="color">10 Web Development</li>
+                <li>10 Yoga Classes</li>
+                <li class="color">20 Virtual Assistant</li>
+                <li>10 System Analyst</li>
+                <li class="color">5 WordPress</li>
+                <li>10 Programming</li>
+              </ul>
+              <a href="https://github.com/samuelbetio/dGitFile" class="btn btn-select-plan btn-sm">Select Plan</a></div>
+          </div>
+          <div class="col-md-3 animate-box">
+            <div class="price-box">
+              <h2 class="pricing-plan">Basic</h2>
+              <div class="price"><sup class="currency">$</sup>27<small>/month</small></div>
+              <ul class="classes">
+                <li>15 English 101</li>
+                <li class="color">10 Web Development</li>
+                <li>10 Yoga Classes</li>
+                <li class="color">20 Virtual Assistant</li>
+                <li>10 System Analyst</li>
+                <li class="color">5 WordPress</li>
+                <li>10 Programming</li>
+              </ul>
+              <a href="https://github.com/samuelbetio/dGitFile" class="btn btn-select-plan btn-sm">Select Plan</a></div>
+          </div>
+          <div class="col-md-3 animate-box">
+            <div class="price-box popular">
+              <h2 class="pricing-plan pricing-plan-offer">Pro <span>Best Offer</span></h2>
+              <div class="price"><sup class="currency">$</sup>74<small>/month</small></div>
+              <ul class="classes">
+                <li>15 English 101</li>
+                <li class="color">10 Web Development</li>
+                <li>10 Yoga Classes</li>
+                <li class="color">20 Virtual Assistant</li>
+                <li>10 System Analyst</li>
+                <li class="color">5 WordPress</li>
+                <li>10 Programming</li>
+              </ul>
+              <a href="https://github.com/samuelbetio/dGitFile" class="btn btn-select-plan btn-sm">Select Plan</a></div>
+          </div>
+          <div class="col-md-3 animate-box">
+            <div class="price-box">
+              <h2 class="pricing-plan">Unlimited</h2>
+              <div class="price"><sup class="currency">$</sup>140<small>/month</small></div>
+              <ul class="classes">
+                <li>15 English 101</li>
+                <li class="color">10 Web Development</li>
+                <li>10 Yoga Classes</li>
+                <li class="color">20 Virtual Assistant</li>
+                <li>10 System Analyst</li>
+                <li class="color">5 WordPress</li>
+                <li>10 Programming</li>
+              </ul>
+              <a href="https://github.com/samuelbetio/dGitFile" class="btn btn-select-plan btn-sm">Select Plan</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="fh5co-started" style="background-image:url(../assets/images/img_bg_2.jpg);">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row animate-box">
+        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+          <h2>Lets Get Started</h2>
+          <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+        </div>
+      </div>
+      <div class="row animate-box">
+        <div class="col-md-8 col-md-offset-2 text-center">
+          <p><a href="https://github.com/samuelbetio/dGitFile" class="btn btn-default btn-lg">Create A Free Course</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="fh5co-footer" role="contentinfo">
+    <div class="container">
+      <div class="row row-pb-md">
+        <div class="col-md-3 fh5co-widget">
+          <h4>About Learning</h4>
+          <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+          <h4>Learning</h4>
+          <ul class="fh5co-footer-links">
+            <li><a href="https://github.com/samuelbetio/dGitFile">Course</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Blog</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Contact</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Terms</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Meetups</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+          <h4>Learn &amp; Grow</h4>
+          <ul class="fh5co-footer-links">
+            <li><a href="https://github.com/samuelbetio/dGitFile">Blog</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Privacy</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Testimonials</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Handbook</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Held Desk</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+          <h4>Engage us</h4>
+          <ul class="fh5co-footer-links">
+            <li><a href="https://github.com/samuelbetio/dGitFile">Marketing</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Visual Assistant</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">System Analysis</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Advertise</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+          <h4>Legal</h4>
+          <ul class="fh5co-footer-links">
+            <li><a href="https://github.com/samuelbetio/dGitFile">Find Designers</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Find Developers</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Teams</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">Advertise</a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile">API</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row copyright">
+        <div class="col-md-12 text-center">
+          <p><small class="block">&copy; All Rights Reserved.</small> <small class="block">Designed by <a target="_blank" rel="nofollow noopener" href="//freehtml5.co/">FreeHTML5.co</a> Demo Images: Unsplash</small></p>
+          <ul class="fh5co-social-icons">
+            <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-twitter"></i></a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-facebook"></i></a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-linkedin"></i></a></li>
+            <li><a href="https://github.com/samuelbetio/dGitFile"><i class="icon-dribbble"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+<div class="gototop js-top"><a href="javascript:void(0)" class="js-gotop"><i class="icon-arrow-up"></i></a></div>
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/jquery.easing.1.3.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery.waypoints.min.js"></script>
+<script src="../assets/js/jquery.stellar.min.js"></script>
+<script src="../assets/js/owl.carousel.min.js"></script>
+<script src="../assets/js/jquery.countTo.js"></script>
+<script src="../assets/js/jquery.magnific-popup.min.js"></script>
+<script src="../assets/js/magnific-popup-options.js"></script>
+<script src="../assets/js/main.js"></script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">
+var gaProperty = 'UA-120201777-1';var disableStr = 'ga-disable-' + gaProperty;if (document.cookie.indexOf(disableStr + '=true') > -1) {window[disableStr] = true;}
+function gaOptout(){document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2045 23:59:59 UTC; path=/';window[disableStr] = true;alert('Google Tracking has been deactivated');}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-120201777-1', 'auto');ga('set', 'anonymizeIp', true);ga('send', 'pageview');
+</script>
+</body>
+</html>
